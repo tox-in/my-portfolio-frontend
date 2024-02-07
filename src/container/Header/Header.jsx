211f2805467f18f8a41349d6214b375b4776c7bc
tox-in <habimanat78@gmail.com>
2024-02-07 20:14:15 +0200

    added new changes in Header.jsx

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+import { images } from '../../constants'
+import './Header.scss'
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className='app__header app__flex'>
+      <motion.div
+      whileInView={{ x: [-100, 0], opacity: [0, 1]}}
+      transition={{ duration: 0.5 }}
+      className='app__header-info'
+      >
+        <div className="app__header-badge">
+          <div className="badge-cmp  app__flex">
+            <span></span>
+            
+          </div>
+        </div>
+      </motion.div>
     </div>
   )
 }
